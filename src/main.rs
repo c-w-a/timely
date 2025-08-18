1f958e3fc9f26190d8998516875f5d7d3b4e05c3
c <c@cs-MacBook-Air.local>
2025-08-17 20:01:03 -0700

    Update main.rs

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,18 @@
+// written by cwa on 17/aug/2025
+
 use anyhow::Result;
+use chrono::{DateTime, Utc};
+
+mod utc;
 
 fn main() -> Result<()> {
+    let rt = tokio::runtime::Runtime::new()?;
+    let utc_now: DateTime<Utc> = rt.block_on(utc::fetch_current_utc_datetime(
+        111, // per-host timeout ms
+        19,  // cutoff in ms
+        3,   // minimum keep
+    ))?;
+    println!("current utc datetime:     {utc_now}");
+
     Ok(())
 }
